fb734bc16aea8fca380b22dd6fe174bea4fe4ae9
waterscentedcandles <audreycuttilan5@gmail.com>
2025-04-04 18:38:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ function playTape(section) {
         Here’s my portfolio — a space for experimenting, exploring, and expressing.
       </p>
     `,
-  projects: `
-  <script>window.location.href = 'designs.html';</script>
-`,
+ projects: `<script>window.location.href = 'designs.html';</script>`,
+    
     contact: `
       <h2 class="neon-text">Contact</h2>
      <p class="loading-text">Hit me up: <br><a href="mailto:audreycuttilan5@gmail.com">audreycuttilan5@gmail.com</a></p> 
